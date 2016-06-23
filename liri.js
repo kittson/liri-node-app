4509f0ba2d06295056c32fee4f82e78e2e19463a
kittson <gazorna@gmail.com>
2016-06-23 18:24:52 -0500

    adding modified liri.js

--- liri.js
+++ liri.js
@@ -1,9 +1,10 @@
-// https://apps.twitter.com/app/12520381  twitter app key
-// Consumer Key (API Key)	nuR8dK5LN0sMIoF3GRu91Of2Z
-// Consumer Secret (API Secret)	MylhSSmbn0UEDNWn9VOF2LzLrYiOaphGsUa6rrj0tQ4m7nzFAw
-// Owner ID	1033799005
-// used http://expressjs.com/en/starter/hello-world.html
 
+
+
+var keys = require('./keys.js');
+var twitter = require('twitter');
+var request = require('request');
+var spotify = require('spotify');
 
 var express = require('express');
 var appTest = express();
@@ -15,3 +16,9 @@ appTest.get('/',function (req, res) {
 appTest.listen(3000, function() {
 	console.log('Eampale app listening on port 3000');
 });
+
+// var queryTwitURL = "https://api.twitter.com/1.1/search/tweets.json";
+// $.ajax({url: queryTwitURL, method: 'GET'}).done(function(response) {
+// 	var twitResults = response;
+// });
+
