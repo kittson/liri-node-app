b08411615d1819f3fbfb7b89fa86048ce71525e3
kittson <gazorna@gmail.com>
2016-06-23 22:30:05 -0500

    pushing up

--- keys.js
+++ keys.js
@@ -1,8 +1,14 @@
+// https://apps.twitter.com/app/12520381  twitter app key
+// Consumer Key (API Key)	nuR8dK5LN0sMIoF3GRu91Of2Z
+// Consumer Secret (API Secret)	MylhSSmbn0UEDNWn9VOF2LzLrYiOaphGsUa6rrj0tQ4m7nzFAw
+// Owner ID	1033799005
+// used http://expressjs.com/en/starter/hello-world.htm
+
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'nuR8dK5LN0sMIoF3GRu91Of2Z',
+  consumer_secret: 'MylhSSmbn0UEDNWn9VOF2LzLrYiOaphGsUa6rrj0tQ4m7nzFAw',
+  access_token_key: '1033799005-JLfGO07j1fFOEzc6HiTObNxCEV2N7TmTfTTktTu',
+  access_token_secret: 'QQ1r1e9JIXRHso05sxMmDyX5VNcjvWUtgtBdmdlfT6lhB',
 }
--- liri.js
+++ liri.js
@@ -17,6 +17,14 @@ appTest.listen(3000, function() {
 	console.log('Eampale app listening on port 3000');
 });
 
+var twit = new twit({
+  "consumer_secret": twitterKeys.consumer_secret,
+  "consumer_key": twitterKeys.consumer_key,
+  "access_token": twitterKeys.access_token,
+  "access_token_secret": twitterKeys.access_token_secret
+});
+
+var stream = twit.stream("user", { screen_name: "gazorna"});
 // var queryTwitURL = "https://api.twitter.com/1.1/search/tweets.json";
 // $.ajax({url: queryTwitURL, method: 'GET'}).done(function(response) {
 // 	var twitResults = response;
