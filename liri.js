5931ca79109c804a6c196b6872bba96ec7b58f02
kittson <gazorna@gmail.com>
2016-06-24 21:50:26 -0500

    done for the evening

--- liri.js
+++ liri.js
@@ -1,12 +1,68 @@
-
-
-
+//homework assignment liri-siri, week 10
+//Booth Kittson
 var keys = require('./keys.js');
-var twitter = require('twitter');
+var twit = require('twitter');
 var request = require('request');
 var spotify = require('spotify');
 
-var express = require('express');
+//gets the twitter api key information from keys.js
+var client = new twit({
+  //"consumer_secret": keys.consumer_secret,  
+  //"consumer_key": keys.consumer_key,
+  //"access_token": keys.access_token,
+  //"access_token_secret": keys.access_token_secret
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  consumer_key: keys.twitterKeys.consumer_key,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret
+});
+
+var params = {screen_name: 'gazorna'}; //twitter username
+
+var argToDo = process.argv[2];
+
+switch (argToDo) {
+	case 'my-tweets': 
+		client.get('statuses/user_timeline', params, function(error, tweets, response){
+			if (!error) {
+			console.log(tweets[0].text);
+			}
+		});           		
+	break;
+
+	case 'spotify-this': 
+		spotify.lookup({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+			if ( err ) {
+			  console.log('Error occurred: ' + err);
+			  return;
+			}
+	 	console.log(data);
+	    // Do something with 'data' 
+	 	});
+	break;
+
+	case 'movie-this': 
+	var myMovie = "Elf";
+ 	request('http://www.omdbapi.com?t=' + myMovie, function (error, response, body) {
+	 	if (!error && response.statusCode == 200) {
+	    	//console.log(body); // Show the HTML for the Google homepage.
+			var movieData = JSON.parse(body);
+			console.log(movieData); 
+	 	}
+	})
+	break;
+
+	case 'do-what': 
+		results = firstNum/secondNum;
+		console.log(results);
+	break;	
+
+	default:
+	console.log("Sorry, didn't understand input. Try again!\n" +
+	"Usage: node liri.js [my-tweets | spotify-this | movie-this | do-what] \n"); 
+};//switch
+
+/*var express = require('express');
 var appTest = express();
 
 appTest.get('/',function (req, res) {
@@ -15,18 +71,4 @@ appTest.get('/',function (req, res) {
 
 appTest.listen(3000, function() {
 	console.log('Eampale app listening on port 3000');
-});
-
-var twit = new twit({
-  "consumer_secret": twitterKeys.consumer_secret,
-  "consumer_key": twitterKeys.consumer_key,
-  "access_token": twitterKeys.access_token,
-  "access_token_secret": twitterKeys.access_token_secret
-});
-
-var stream = twit.stream("user", { screen_name: "gazorna"});
-// var queryTwitURL = "https://api.twitter.com/1.1/search/tweets.json";
-// $.ajax({url: queryTwitURL, method: 'GET'}).done(function(response) {
-// 	var twitResults = response;
-// });
-
+});*/
